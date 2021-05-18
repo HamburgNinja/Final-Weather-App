50124f51230f5d26b8b2ea96e7b174756d0e65d3
HamburgNinja <nina.drescher@gmail.com>
2021-05-18 14:49:40 +0200

    Deleted conversion to Fahrenheit & added Feels like info

--- src/app.js
+++ src/app.js
@@ -77,7 +77,6 @@ function showForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "b409f6a940ab5609bc1ca05aa3fc68e6";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showForecast);
@@ -92,6 +91,9 @@ function showWeather(response) {
   document.querySelector("#country").innerHTML = response.data.sys.country;
   document.querySelector("#weather-description").innerHTML =
     response.data.weather[0].main;
+  document.querySelector("#feelsLikeTemp").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     3.6 * response.data.wind.speed
@@ -119,31 +121,7 @@ function handleSubmit(event) {
   search(searchInputElement.value);
 }
 
-function convertToFahrenheit(event) {
-  event.preventDefault();
-  celsiusConversion.classList.remove("active");
-  fahrenheitConversion.classList.add("active");
-  let celsius = document.querySelector("#current-temp").innerHTML;
-  let fahrenheit = Math.round((celsius * 9) / 5 + 32);
-  document.querySelector("#current-temp").innerHTML = fahrenheit;
-}
-
-function convertToCelsius(event) {
-  event.preventDefault();
-  celsiusConversion.classList.add("active");
-  fahrenheitConversion.classList.remove("active");
-  let fahrenheit = document.querySelector("#current-temp").innerHTML;
-  let celsius = Math.round(((fahrenheit - 32) * 5) / 9);
-  document.querySelector("#current-temp").innerHTML = celsius;
-}
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
-
-let fahrenheitConversion = document.querySelector("#unit-fahrenheit");
-fahrenheitConversion.addEventListener("click", convertToFahrenheit);
-
-let celsiusConversion = document.querySelector("#unit-celsius");
-celsiusConversion.addEventListener("click", convertToCelsius);
 
 search("Hamburg");
