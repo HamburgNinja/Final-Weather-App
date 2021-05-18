808e4d49f08480a8804bf137e99ac262100f4268
HamburgNinja <nina.drescher@gmail.com>
2021-05-18 12:40:10 +0200

    JS: implanted Forecast API

--- src/app.js
+++ src/app.js
@@ -22,7 +22,9 @@ function formatDate(timestamp) {
   return `${day} | ${hours}:${minutes}`;
 }
 
-function weatherForecast() {
+function showForecast(response) {
+  console.log(response.data.daily);
+
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -50,6 +52,13 @@ function weatherForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "b409f6a940ab5609bc1ca05aa3fc68e6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
+}
+
 function showWeather(response) {
   let weatherIcon = document.querySelector("#current-weather-icon");
   document.querySelector("#current-temp").innerHTML = Math.round(
@@ -71,6 +80,7 @@ function showWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   weatherIcon.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -113,4 +123,3 @@ let celsiusConversion = document.querySelector("#unit-celsius");
 celsiusConversion.addEventListener("click", convertToCelsius);
 
 search("Hamburg");
-weatherForecast();
